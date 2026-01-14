93ac2741590da9e4e6fb9484690fb4b54e95d028
gall <daniel.gallagher.js@gmail.com>
2026-01-14 11:54:34 +0100

    all models use trf

--- src/reef/obfuscators/spacy_registry.py
+++ src/reef/obfuscators/spacy_registry.py
@@ -6,11 +6,12 @@ _models = {}
 
 
 def get_spacy_nlp(pipeline="ner") -> Any:
+    spacy.prefer_gpu()
     if pipeline not in _models:
         if pipeline == "ner":
             _models[pipeline] = spacy.load("en_core_web_trf", disable=["parser"])
         elif pipeline == "lemma":
-            _models[pipeline] = spacy.load("en_core_web_trf", disable=["parser", "ner"])
+            _models[pipeline] = spacy.load("en_core_web_trf", disable=["parser", "ner", "textcat"])
         elif pipeline == "full":
             _models[pipeline] = spacy.load("en_core_web_trf")
         elif pipeline == "de":
