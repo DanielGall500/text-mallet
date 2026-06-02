7b64ed23871e66fa4fdcef84fe649ee2b37bc653
gall <daniel.gallagher.js@gmail.com>
2026-06-02 16:28:19 +0200

    added sent_tokenize

--- src/tmallet/utils/__init__.py
+++ src/tmallet/utils/__init__.py
@@ -1,4 +1,4 @@
-from tmallet.utils.spacy_registry import SpaCyInterface, LangConfig
-from tmallet.utils.helper import flatten_dict
+from tmallet.utils.helper import flatten_dict, sent_tokenize
+from tmallet.utils.spacy_registry import LangConfig, SpaCyInterface
 
-__all__ = ["SpaCyInterface", "LangConfig", flatten_dict]
+__all__ = ["SpaCyInterface", "LangConfig", flatten_dict, sent_tokenize]
